bb7cc32cce3e282fab8c675f2a0241c152f9d1cb
Aleksa <obradovic.aleksa@gmail.com>
2022-08-18 20:12:33 +0200

    Functional update form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Setup from './tutorial/1-useState/setup/5-useState-counter';
+import Final from './tutorial/1-useState/setup/3-useState-array';
 
 
 function App() {
   return (
     <div className='container'>
-      <Setup />
+      <Final />
     
     </div>
   )
