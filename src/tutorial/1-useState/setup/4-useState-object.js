f82d7643e3c5d6d7807acdfd4fd7fec3b955691f
Aleksa <obradovic.aleksa@gmail.com>
2022-08-18 12:25:31 +0200

    useState - Objwct example

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,31 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    message: 'random message'
+  });
+
+  // console.log(person);
+  const changeMessage = () => {
+    setPerson({...person, message:'hello world'});
+  }
+
+
+  return (
+    <>
+      <h2>{person.name}</h2>;
+      <h2>{person.age}</h2>;
+      <h2>{person.message}</h2>
+      <button className="btn" onClick={changeMessage}>
+
+        change message
+      </button>
+    </>
+  );
+
+
 };
 
 export default UseStateObject;
